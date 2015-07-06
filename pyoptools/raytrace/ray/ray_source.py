ed85dcf27274e4a1dab7d65cd49b70f8afa7a3c3
Pablo Jais <pablo.jais@satellogic.com>
2015-07-06 14:48:25 -0300

    Added parallel_beam_list

--- pyoptools/raytrace/ray/ray_source.py
+++ pyoptools/raytrace/ray/ray_source.py
@@ -12,22 +12,22 @@ from pyoptools.raytrace.ray import Ray
 
 
 def parallel_beam_c(origin=(0.,0.,0.),direction=(0.,0.,0.),size=(1.,1.),num_rays=(10,10),wavelength=0.58929, label=""):
-    """Cartesian grid parallel beam 
-    
-    This function creates a parallel beam, where the rays are organized in a 
-    cartesian grid. 
-    
+    """Cartesian grid parallel beam
+
+    This function creates a parallel beam, where the rays are organized in a
+    cartesian grid.
+
     Parameters:
-    
-    
+
+
     *origin*
-        Tuple with the coordinates of the central ray origin 
+        Tuple with the coordinates of the central ray origin
     *direction*
         Tuple with the rotation of the beam arround the XYZ axes.
     *size*
         Tuple with the beam's width and the height.
     *num_rays*
-        Tuple (nx,ny) containing the number of rays used to create the beam. 
+        Tuple (nx,ny) containing the number of rays used to create the beam.
     *label*
         String used to identify the ray source
     """
@@ -40,7 +40,7 @@ def parallel_beam_c(origin=(0.,0.,0.),direction=(0.,0.,0.),size=(1.,1.),num_rays
     # note modify this to use traits
     dx=float(dx)
     dy=float(dy)
-    
+
     for ix in range(nx):
         for iy in range(ny):
             x=-dx/2.+dx*ix/(nx-1)
@@ -52,25 +52,25 @@ def parallel_beam_c(origin=(0.,0.,0.),direction=(0.,0.,0.),size=(1.,1.),num_rays
 
 def parallel_beam_p(origin=(0.,0.,0.),direction=(0.,0.,0),radius=0.5, num_rays=(5,10),wavelength=0.58929, label=""):
     """Polar grid parallel beam
-    
-    This function creates a parallel beam, where the rays are organized in a 
-    polar grid. 
-    
+
+    This function creates a parallel beam, where the rays are organized in a
+    polar grid.
+
     Parameters:
-   
-    
+
+
     *origin*
-        Tuple with the coordinates of the central ray origin 
+        Tuple with the coordinates of the central ray origin
     *direction*
         Tuple with the rotation of the beam arround the XYZ axes.
     *r*
-        Beam radious 
+        Beam radious
     *num_rays*
-        Tuple (nr,na) containing the number of rays used to create the beam. 
+        Tuple (nr,na) containing the number of rays used to create the beam.
     *label*
         String used to identify the ray source
     """
-    
+
     ret_val=[Ray(pos=(0,0,0),dir=(0,0,1),wavelength=wavelength, label=label).ch_coord_sys_inv(origin,direction)]
     nr,nt=num_rays
     for r_ in range(1,nr):
@@ -86,22 +86,22 @@ def parallel_beam_p(origin=(0.,0.,0.),direction=(0.,0.,0),radius=0.5, num_rays=(
 
 def point_source_c(origin=(0.,0.,0.),direction=(0.,0.,0),span=(pi/8,pi/8)\
                      ,num_rays=(10,10),wavelength=0.58929, label=""):
-    """Point source, with a cartesian beam distribution 
-    
-    This function creates a point source, where the rays are organized in a 
-    cartesian grid. 
-    
+    """Point source, with a cartesian beam distribution
+
+    This function creates a point source, where the rays are organized in a
+    cartesian grid.
+
     Parameters:
-    
-    
+
+
     *origin*
-        Tuple with the coordinates of the central ray origin 
+        Tuple with the coordinates of the central ray origin
     *direction*
         Tuple with the rotation of the beam arround the XYZ axes.
     *span*
         Tuple angular size of the ray pencil.
     *num_rays*
-        Tuple (nx,ny) containing the number of rays used to create the beam. 
+        Tuple (nx,ny) containing the number of rays used to create the beam.
     *label*
         String used to identify the ray source
     """
@@ -109,12 +109,12 @@ def point_source_c(origin=(0.,0.,0.),direction=(0.,0.,0),span=(pi/8,pi/8)\
 
     nx,ny=num_rays
     dx,dy=span
-        
+
     for ix in range(nx):
         for iy in range(ny):
             if nx!=1: tx=-dx/2.+dx*ix/(nx-1)
             else: tx=0.
-            
+
             if ny!=1: ty=-dy/2.+dy*iy/(ny-1)
             else: ty=0.
             temp_ray=Ray(pos=(0,0,0),
@@ -128,15 +128,15 @@ def point_source_c(origin=(0.,0.,0.),direction=(0.,0.,0),span=(pi/8,pi/8)\
 
 def point_source_p(origin=(0.,0.,0.),direction=(0.,0.,0),span=pi/8,num_rays=(10,10),wavelength=0.58929, label=""):
     """Point source, with a polar beam distribution
-    
-    This function creates a point source, where the rays are organized in a 
-    polar grid. 
-    
+
+    This function creates a point source, where the rays are organized in a
+    polar grid.
+
     Parameters:
-    
-    
+
+
     *origin*
-        Tuple with the coordinates of the central ray origin 
+        Tuple with the coordinates of the central ray origin
     *direction*
         Tuple with the rotation of the beam arround the XYZ axes.
     *span*
@@ -161,15 +161,15 @@ def point_source_p(origin=(0.,0.,0.),direction=(0.,0.,0),span=pi/8,num_rays=(10,
 
 def point_source_r(origin=(0.,0.,0.),direction=(0.,0.,0),span=pi/8,num_rays=100,wavelength=0.58929, label=""):
     """Point source, with a ranrom beam distribution
-    
-    This function creates a point source, where the rays are organized in a 
-    random grid. 
-    
+
+    This function creates a point source, where the rays are organized in a
+    random grid.
+
     Parameters:
-   
-    
+
+
     *origin*
-        Tuple with the coordinates of the central ray origin 
+        Tuple with the coordinates of the central ray origin
     *direction*
         Tuple with the rotation of the beam arround the XYZ axes.
     *span*
@@ -181,10 +181,49 @@ def point_source_r(origin=(0.,0.,0.),direction=(0.,0.,0),span=pi/8,num_rays=100,
     """
 
     ret_val=[]
-    
+
     for n_ in range(num_rays):
         rx=normal(0,span)
         ry=normal(0,span)
         temp_ray=Ray(pos=(0,0,0),dir=(0,0,1),wavelength=wavelength, label=label).ch_coord_sys_inv((0,0,0),(rx,ry,0))
         ret_val.append(temp_ray.ch_coord_sys_inv(origin,direction))
+    return ret_val
+
+
+def parallel_beam_list(x, y, origin=(0.,0.,0.), direction=(0.,0.,0), wavelength=0.58929, label=""):
+    """Custom grid parallel beam
+
+    This function creates a parallel beam, where the rays are located in
+    coordinates specified in a list.
+
+    Parameters:
+
+
+    *x*
+        X coordinate(s) of the rays
+    *y*
+        Y coordinate(s) of the rays
+    *origin*
+        Tuple with the coordinates of the (0, 0, 0) point
+    *direction*
+        Tuple with the rotation of the beam arround the XYZ axes.
+    *wavelength*
+        Wavelength of the rays
+    *label*
+        String used to identify the ray source
+    """
+    islist_x = hasattr(x, '__len__')
+    islist_y = hasattr(y, '__len__')
+    if not islist_x and not islist_y:
+        x, y = (x, ), (y, )
+    elif not islist_x:
+        x, y = repeat(x, len(y)), y
+    elif not islist_y:
+        x, y = x, repeat(y, len(x))
+    else:
+        assert(len(x)==len(y))
+    ret_val = []
+    for _x, _y in zip(x, y):
+        ret_val.append(Ray(pos=(_x,_y,0), dir=(0,0,1), wavelength=wavelength,
+            label=label).ch_coord_sys_inv(origin,direction))
     return ret_val
